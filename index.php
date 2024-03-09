cc8b1d0209a085298442f0574294404a7f4c59f9
EuCaSe <eucase.02@gmail.com>
2024-03-09 14:23:45 +0800

    My First Commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+    Eunice Serrano
+    <p>ADET BSIT</p>    
+</head>
+<body>
+<?php
+
+$x = 10;  
+$y = 6;
+
+echo $x + $y;
+ 
+
+$x = 50;
+$x -= 30;
+
+echo $x;
+
+
+$x = 5;
+$x *= 6;
+
+echo $x;
+
+echo $x * $y;
+
+
+$x = 10;
+$x /= 5;
+
+echo $x;
+
+MODULUS
+$x = 15;
+$x %= 4;
+
+echo $x;
+?>   
+</body>
+</html>
